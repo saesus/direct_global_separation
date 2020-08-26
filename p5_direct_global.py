Fixed the intensity gradient - it was backwards Removed depricated functions

p5_direct_global.py
@@ -129,6 +129,7 @@ def draw():
     image(gradients[2], (0, height + (gradient_height * 2)))
     image(gradients[3], (0, height + (gradient_height * 3)))
 
+    # Combines the images for the resultant image based on user selection
     if comb:
         if comb == 1:
             result = processing.combine_images(ld, lg)
@@ -145,6 +146,8 @@ def draw():
             combined_image = processing.convert_cv2_image(result.astype('uint8'), 'BGR')
             image(combined_image, (width * 3, 0),
                   (width, height))
+
+    # Draws circles where the user has clicked on each of the gradients
     if red_elipse:
         ellipse((red_elipse[0], red_elipse[1]), 25, 25)
     if green_elipse:
@@ -199,7 +202,7 @@ def mouse_pressed():
             print(filter_blue)
             draw()
         elif mouse_y < height + (4 * gradient_height):
-            filter_int = mouse_x / int(width * 4)
+            filter_int = 1 - (mouse_x / int(width * 4))
             int_elipse = [mouse_x, mouse_y]
             print(filter_int)
             draw()
@@ -211,6 +214,7 @@ def generate_filtered_image(image):
     global filter_int
     global filter_img
 
+    # Inits a filter image with no mixing
     filter_img = np.full((len(ld), len(ld[0]), 3), (filter_red, filter_green, filter_blue), np.uint8)
     fused_img = cv2.addWeighted(image, (1 - filter_int), filter_img, filter_int, 0)
 

utils/processing.py
@@ -22,13 +22,6 @@ def combine_images(img1, img2):
     img = norm_img.astype(np.uint8)
 
     return img
-    # return np.mean([img1, img2], axis=0)
-
-
-def compare_images(image1, image2):
-    if np.array_equal(image1, image2):
-        return True
-    return False
 
 
 def convert_cv2_image(cv2_image, colorspace):
@@ -99,17 +92,6 @@ def direct_global_separation(video, video_name, width, height):
     return(ld, lg)
 
 
-def get_minmax(frame, lmax, lmin, lmax_g, lmin_g):
-    frame_g = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    for x in range(len(frame_g)):
-        for y in range(len(frame_g[0])):
-            if frame_g[x][y] > lmax_g[x][y]:
-                lmax[x][y] = frame[x][y]
-            elif frame_g[x][y] < lmin_g[x][y]:
-                lmin[x][y] = frame[x][y]
-    return lmax, lmin
-
-
 def scaling_ratio(frame_size, height, width):
     """Returns a ratio to scale an image to 255 pixels
     wide/high
@@ -152,6 +134,7 @@ def generate_color(width, height):
         intensity[:,pos,0] = 255*(pos/width)
         intensity[:,pos,1] = 255*(pos/width)
         intensity[:,pos,2] = 255*(pos/width)
+
     rgb = [convert_cv2_image(r, 'BGR'), convert_cv2_image(g, 'BGR'), convert_cv2_image(b, 'BGR'), convert_cv2_image(intensity, 'BGR')]
     return rgb
 